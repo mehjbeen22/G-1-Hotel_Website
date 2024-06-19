34edf9721935f4ae4d87a33acf5509d9395f7f64
Pooja Sawant <poojasawant22@navgurukul.org>
2024-06-19 15:13:07 +0530

    Created Footer and Newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hotel from './pages/Hotel';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { hotelData } from './data/hotelData';
 import BookingForm from './pages/BookingForm';
+import FooterCom from './pages/FooterCom';
 
 const App = () => {
   return (
@@ -17,8 +18,13 @@ const App = () => {
         <Route path="/hotel/:hotelId" element={<Hotel />} />
         <Route path='/BookingForm' element={BookingForm}/>
       </Routes>
+      <div>
+        <FooterCom />
+      </div>
     </Router>
   );
 };
 
 export default App;
+
+
